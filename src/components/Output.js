148653aa1ed2c866aa98720bfab88b7599d18fe0
asd0999 <obelixotic@gmail.com>
2021-03-27 18:07:42 -0400

    miles and energy required calculations

--- src/components/Output.js
+++ src/components/Output.js
@@ -9,7 +9,10 @@ function Output(props) {
     let b2_a = 0;
     let b2_b = 0;
     if (rate && miles && hours && hours !== "none") {
+      // we have all three inputs
       console.log(loadprofile);
+
+      // rate calculations
       if (rate === "A") {
         b1 = (totalLoad * 0.15).toFixed(2);
         setOutput(`b1 for rate A: ${b1}`);
@@ -27,7 +30,15 @@ function Output(props) {
         b1 = b1.toFixed(2);
         setOutput(`b1 for rate B: ${b1}`);
       }
+
+      // miles calculations
+      let energyRequired = miles * 0.3;
+      console.log(energyRequired);
+      let hoursOfCharging = Math.ceil(energyRequired / (365 * 7.2));
+      console.log(hoursOfCharging);
     }
+
+    // hours calculations
   }, [rate, miles, hours]);
 
   return <div>{output}</div>;
