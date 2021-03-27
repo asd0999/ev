61bc293697c4f380b596c749fed2d290d6440c49
asd0999 <obelixotic@gmail.com>
2021-03-27 14:38:45 -0400

    parsing data from csv

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as d3 from "d3";
 import Hours from "./components/Hours";
 import Miles from "./components/Miles";
 import Rates from "./components/Rates";
+import data from "./lp.csv";
 
 function App() {
   const [rate, setRate] = useState("");
   const [miles, setMiles] = useState(0);
   const [hours, setHours] = useState(null);
+  let rawDataArr = [];
+  let parsedData = [];
+  const [loadprofile, setLoadprofile] = useState([]);
+
+  useEffect(() => {
+    d3.csv(data, function (data) {
+      const datetime = data["Date/Time"];
+      const [date, a, time] = datetime.trim().split(" ");
+      const consumption = parseFloat(
+        data["Electricity:Facility [kWh](Hourly)"]
+      );
+      rawDataArr = [...rawDataArr, [date, time, consumption]];
+    }).then((res) => {
+      for (let i = 0; i < 365; i++) {
+        parsedData[i] = [];
+        for (let j = i * 24; j < i * 24 + 24; j++) {
+          parsedData[i].push(rawDataArr[j]);
+        }
+      }
+      // console.log(parsedData);
+      setLoadprofile(parsedData);
+    });
+  }, []);
 
   return (
     <div className="container">
