f8e97479bcda522190aeb61a7b5e9e070a6ee46c
asd0999 <obelixotic@gmail.com>
2021-03-27 16:08:31 -0400

    trigger calculation when all user inputs are received/changed

--- src/App.js
+++ src/App.js
@@ -12,32 +12,47 @@ function App() {
   let rawDataArr = [];
   let parsedData = [];
   const [loadprofile, setLoadprofile] = useState([]);
+  const [totalLoad, setTotalLoad] = useState(0);
 
   useEffect(() => {
+    let totalconsumption = 0;
+
     d3.csv(data, function (data) {
       const datetime = data["Date/Time"];
       const [date, a, time] = datetime.trim().split(" ");
       const consumption = parseFloat(
         data["Electricity:Facility [kWh](Hourly)"]
       );
+      totalconsumption += consumption; // save data to var instead of updating state 8760 times
       rawDataArr = [...rawDataArr, [date, time, consumption]];
     }).then((res) => {
       for (let i = 0; i < 365; i++) {
         parsedData[i] = [];
         for (let j = i * 24; j < i * 24 + 24; j++) {
-          parsedData[i].push(rawDataArr[j]);
+          parsedData[i].push(rawDataArr[j]); // save data to var instead of updating state 8760 times
         }
       }
-      // console.log(parsedData);
+
+      //once all data is parsed, save it to state
       setLoadprofile(parsedData);
+      setTotalLoad(totalconsumption);
     });
   }, []);
 
+  useEffect(() => {
+    if (rate && miles && hours && hours !== "none") {
+      console.log("perform calculation");
+    }
+  }, [rate, miles, hours]);
+
   return (
     <div className="container">
-      <Rates rate={rate} setRate={setRate} />
-      <Miles miles={miles} setMiles={setMiles} />
-      <Hours hours={hours} setHours={setHours} />
+      <div className="input">
+        <Rates rate={rate} setRate={setRate} />
+        <Miles miles={miles} setMiles={setMiles} />
+        <Hours hours={hours} setHours={setHours} />
+      </div>
+      <div className="output"></div>
     </div>
   );
 }
